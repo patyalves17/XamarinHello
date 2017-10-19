54ceb060a33c149bc2d3b2116a46833471abb910
patyalves17 <patriciaalves10@gmail.com>
2017-10-18 22:15:00 -0200

    helloWord

---
+++ XF_Hello/XF_Hello/XF_Hello/DataBindingPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XF_Hello
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DataBindingpage : ContentPage
+    {
+        public DataBindingpage()
+        {
+            Aluno aluno = InitAluno("Anderson Silva", "anderson@fiap.com");
+            BindingContext = aluno;
+
+            InitializeComponent();
+        }
+
+        private Aluno InitAluno(string nome, string email)
+        {
+            return new Aluno()
+            {
+                Id = Guid.NewGuid(),
+                Nome = nome,
+                Email = email
+            };
+        }
+    }
+}
